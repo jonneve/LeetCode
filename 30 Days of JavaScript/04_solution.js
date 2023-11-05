6fa14578a5a960b130eb756be51855dce0ed22d0
Jon <jonneve@hotmail.co.uk>
2023-11-05 09:51:05 +0000

    js solution 4

---
+++ 30 Days of JavaScript/04_solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+
+    // set counter initial value to init
+    var counter = init;
+
+    // functions to increment, decrement and reset, where reset sets counter back to initial init value
+    function increment() {
+        return ++counter;
+    }
+
+    function decrement() {
+        return --counter;
+    }
+
+    function reset() {
+        counter = init;
+        return counter;
+    }
+
+    // return expected functions
+    return {
+        increment, decrement, reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
