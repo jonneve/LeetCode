13dd4d8a442d938e22241283858973f41c7f11a1
Jon <jonneve@hotmail.co.uk>
2023-11-07 08:20:56 +0000

    js solution 5

---
+++ 30 Days of JavaScript/05_solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+
+    // apply map funciton on array where fn funciton is applied to each element before resultant array is returned
+    return arr.map(fn);
+
+};
